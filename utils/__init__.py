e6054481b704f925887d927edd439ffd6260fbd4
aviadkim <aviad@kimfo-fs.com>
2025-03-23 15:56:15 +0200

    Fix import issue by adding extract_text_from_pdf function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,20 @@
 # utils/__init__.py
 # ייבוא פונקציות שימושיות
 
-# הערה: נשנה את הייבוא כדי שיתאים למבנה הקובץ הנוכחי
-# במקום ייבוא פונקציה ספציפית, נייבא את המחלקה PDFProcessor
 from .pdf_processor import PDFProcessor
+
+# יצירת פונקציית מעטפת לתאימות לאחור
+def extract_text_from_pdf(pdf_path, ocr_enabled=True, lang="heb+eng"):
+    """
+    פונקציית עזר לחילוץ טקסט מקובץ PDF - לצורך תאימות לאחור
+    
+    Args:
+        pdf_path: נתיב לקובץ PDF
+        ocr_enabled: האם להפעיל OCR
+        lang: שפת OCR
+        
+    Returns:
+        str: הטקסט המחולץ
+    """
+    processor = PDFProcessor(ocr_enabled=ocr_enabled, lang=lang)
+    return processor.extract_text(pdf_path)
