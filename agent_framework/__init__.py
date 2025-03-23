0d3e4609d04608896e967900e7d85e3d6639d0f0
aviadkim <aviad@kimfo-fs.com>
2025-03-23 11:49:24 +0200

    Update __init__.py to include new agents

--- agent_framework/__init__.py
+++ agent_framework/__init__.py
@@ -1,5 +1,13 @@
 from .coordinator import AgentCoordinator
 from .table_generator import CustomTableGenerator
 from .nlp_agent import NaturalLanguageQueryAgent
+from .memory_agent import MemoryAgent
+from .analytics_agent import AnalyticsAgent
 
-__all__ = ['AgentCoordinator', 'CustomTableGenerator', 'NaturalLanguageQueryAgent']
+__all__ = [
+    'AgentCoordinator', 
+    'CustomTableGenerator', 
+    'NaturalLanguageQueryAgent',
+    'MemoryAgent',
+    'AnalyticsAgent'
+]
